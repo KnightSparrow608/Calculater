36e4ed3f7bcc6543157f1bae8d28ddc0962872da
NightSparrow608 <m.dtravis21@gmail.com>
2021-11-22 18:30:57 -0600

    update

--- calculator.py
+++ calculator.py
@@ -1,46 +1,12 @@
 from tkinter import * 
 import tkinter as tk
 
+#Variables 
 equation = 0
-interface = tk.Tk()
+tempvar = 0
+function = ""
 
-interface.title("Calculator")
-interface.geometry("315x400")
-
-expression_field = Entry(interface, textvariable=equation)
-expression_field.grid(row=1, columnspan=4, ipadx=100, ipady=25)
-
-equation = StringVar()
-
-button0=tk.Button(interface, text="0", width=10, height=5)
-button0.grid(row=5,column=1)
-button1=tk.Button(interface, text="1", width=10, height=5)
-button1.grid(row=4,column=0)
-button2=tk.Button(interface, text="2", width=10, height=5)
-button2.grid(row=4,column=1)
-button3=tk.Button(interface, text="3", width=10, height=5)
-button3.grid(row=4,column=2)
-button4=tk.Button(interface, text="4", width=10, height=5)
-button4.grid(row=3,column=0)
-button5=tk.Button(interface, text="5", width=10, height=5)
-button5.grid(row=3,column=1)
-button6=tk.Button(interface, text="6", width=10, height=5)
-button6.grid(row=3,column=2)
-button7=tk.Button(interface, text="7", width=10, height=5)
-button7.grid(row=2,column=0)
-button8=tk.Button(interface, text="8", width=10, height=5)
-button8.grid(row=2,column=1)
-button9=tk.Button(interface, text="9", width=10, height=5)
-button9.grid(row=2,column=2)
-btnadd=tk.Button(interface, text="+", width=10, height=5)
-btnadd.grid(row=2,column=3)
-btnsub=tk.Button(interface, text="-", width=10, height=5)
-btnsub.grid(row=3,column=3)
-btnmult=tk.Button(interface, text="*", width=10, height=5)
-btnmult.grid(row=4,column=3)
-btndiv=tk.Button(interface, text="/", width=10, height=5)
-btndiv.grid(row=5,column=3)
-
+#Functions
 def add(): 
     print ("add")
 
@@ -53,4 +19,74 @@ def mult():
 def div():
     print ("div")
 
+def clear():
+    print("Hello")
+
+def submit():
+    print("Submit")
+
+def display():
+    x = 0
+
+#Design GUI
+interface = tk.Tk()
+interface.resizable(width=False, height=False)
+
+interface.title("Calculator")
+interface.geometry("315x400")
+
+expression_field = Entry(interface, textvariable=equation)
+expression_field.grid(row=1, columnspan=4, ipadx=100, ipady=25)
+
+equation = StringVar()
+
+btn0=tk.Button(interface, text="0", width=10, height=5)
+btn0.grid(row=5,column=1)
+
+btn1=tk.Button(interface, text="1", width=10, height=5)
+btn1.grid(row=4,column=0)
+
+btn2=tk.Button(interface, text="2", width=10, height=5)
+btn2.grid(row=4,column=1)
+
+btn3=tk.Button(interface, text="3", width=10, height=5)
+btn3.grid(row=4,column=2)
+
+btn4=tk.Button(interface, text="4", width=10, height=5)
+btn4.grid(row=3,column=0)
+
+btn5=tk.Button(interface, text="5", width=10, height=5)
+btn5.grid(row=3,column=1)
+
+btn6=tk.Button(interface, text="6", width=10, height=5)
+btn6.grid(row=3,column=2)
+
+btn7=tk.Button(interface, text="7", width=10, height=5)
+btn7.grid(row=2,column=0)
+
+btn8=tk.Button(interface, text="8", width=10, height=5)
+btn8.grid(row=2,column=1)
+
+btn9=tk.Button(interface, text="9", width=10, height=5)
+btn9.grid(row=2,column=2)
+
+btnadd=tk.Button(interface, text="+", width=10, height=5, command=add)
+btnadd.grid(row=2,column=3)
+
+btnsub=tk.Button(interface, text="-", width=10, height=5, command=sub)
+btnsub.grid(row=3,column=3)
+
+btnmult=tk.Button(interface, text="*", width=10, height=5, command=mult)
+btnmult.grid(row=4,column=3)
+
+btndiv=tk.Button(interface, text="/", width=10, height=5, command=div)
+btndiv.grid(row=5,column=3)
+
+btnsubmit=tk.Button(interface, text="=", width=10, height=5, command=submit)
+btnsubmit.grid(row=5,column=2)
+
+btnclear=tk.Button(interface, text="Clear", width=10, height=5, command=clear)
+btnclear.grid(row=5,column=0)
+
 interface.mainloop()
+display()
