8bd2062c060107bac13c80a4974b29b42c101dd5
NightSparrow608 <m.dtravis21@gmail.com>
2021-11-23 11:36:03 -0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,30 +3,19 @@ import tkinter as tk
 
 #Variables 
 equation = 0
-tempvar = 0
-function = ""
 
 #Functions
-def add(): 
-    print ("add")
-
-def sub():
-    print ("sub")
-
-def mult():
-    print ("mult")
-
-def div():
-    print ("div")
 
 def clear():
-    print("Hello")
+    print("clear")
 
 def submit():
     print("Submit")
 
-def display():
-    x = 0
+def display(text):
+
+    Entry_field.insert(0, text)
+    return
 
 #Design GUI
 interface = tk.Tk()
@@ -35,51 +24,51 @@ interface.resizable(width=False, height=False)
 interface.title("Calculator")
 interface.geometry("315x400")
 
-expression_field = Entry(interface, textvariable=equation)
-expression_field.grid(row=1, columnspan=4, ipadx=100, ipady=25)
+Entry_field = Entry(interface, textvariable=equation)
+Entry_field.grid(row=1, columnspan=4, ipadx=100, ipady=25)
 
 equation = StringVar()
 
-btn0=tk.Button(interface, text="0", width=10, height=5)
+btn0=tk.Button(interface, text="0", width=10, height=5, command=lambda:display("0"))
 btn0.grid(row=5,column=1)
 
-btn1=tk.Button(interface, text="1", width=10, height=5)
+btn1=tk.Button(interface, text="1", width=10, height=5, command=lambda:display("1"))
 btn1.grid(row=4,column=0)
 
-btn2=tk.Button(interface, text="2", width=10, height=5)
+btn2=tk.Button(interface, text="2", width=10, height=5, command=lambda:display("2"))
 btn2.grid(row=4,column=1)
 
-btn3=tk.Button(interface, text="3", width=10, height=5)
+btn3=tk.Button(interface, text="3", width=10, height=5, command=lambda:display("3"))
 btn3.grid(row=4,column=2)
 
-btn4=tk.Button(interface, text="4", width=10, height=5)
+btn4=tk.Button(interface, text="4", width=10, height=5, command=lambda:display("4"))
 btn4.grid(row=3,column=0)
 
-btn5=tk.Button(interface, text="5", width=10, height=5)
+btn5=tk.Button(interface, text="5", width=10, height=5, command=lambda:display("5"))
 btn5.grid(row=3,column=1)
 
-btn6=tk.Button(interface, text="6", width=10, height=5)
+btn6=tk.Button(interface, text="6", width=10, height=5, command=lambda:display("6"))
 btn6.grid(row=3,column=2)
 
-btn7=tk.Button(interface, text="7", width=10, height=5)
+btn7=tk.Button(interface, text="7", width=10, height=5, command=lambda:display("7"))
 btn7.grid(row=2,column=0)
 
-btn8=tk.Button(interface, text="8", width=10, height=5)
+btn8=tk.Button(interface, text="8", width=10, height=5, command=lambda:display("8"))
 btn8.grid(row=2,column=1)
 
-btn9=tk.Button(interface, text="9", width=10, height=5)
+btn9=tk.Button(interface, text="9", width=10, height=5, command=lambda:display("9"))
 btn9.grid(row=2,column=2)
 
-btnadd=tk.Button(interface, text="+", width=10, height=5, command=add)
+btnadd=tk.Button(interface, text="+", width=10, height=5, command=lambda:display(" + "))
 btnadd.grid(row=2,column=3)
 
-btnsub=tk.Button(interface, text="-", width=10, height=5, command=sub)
+btnsub=tk.Button(interface, text="-", width=10, height=5, command=lambda:display(" - "))
 btnsub.grid(row=3,column=3)
 
-btnmult=tk.Button(interface, text="*", width=10, height=5, command=mult)
+btnmult=tk.Button(interface, text="*", width=10, height=5, command=lambda:display(" * "))
 btnmult.grid(row=4,column=3)
 
-btndiv=tk.Button(interface, text="/", width=10, height=5, command=div)
+btndiv=tk.Button(interface, text="/", width=10, height=5, command=lambda:display(" / "))
 btndiv.grid(row=5,column=3)
 
 btnsubmit=tk.Button(interface, text="=", width=10, height=5, command=submit)
